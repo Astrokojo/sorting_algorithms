2b6269d1ecb75367064b465cc95f9f64ef0bafe0
K.A. Osei-Sarfo <astrokojo@gmail.com>
2023-11-21 11:44:34 +0000

    bubbled

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+/**
+ * bubble_sort - Sorts arrays of int in ascending order
+ * @array: the array
+ * @size: the size of the array
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t step;
+/*loop to access each element of the array - outer loop*/
+	for (step = 0; step <= (size - 1); step++)
+	{
+		int swap_count = 0;
+		size_t i;
+/*loop over each element of the array to compare them - inner loop*/
+		for (i = 0; i < (size - step - 1); i++)
+		{
+			/*compare 2 adjacent elemts and swap into ascending order*/
+			if (array[i] > array[i + 1])
+			{
+				int temp = array[i];
+
+				array[i] = array[i + 1];
+
+				array[i + 1] = temp;
+
+				swap_count += 1;
+				print_array(array, size);
+			}
+		}
+
+		if (swap_count == 0)
+			break;
+	}
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,22 @@
+#include "sort.h"
+/**
+ * sort_sort - Sorts doubly linked lists of int in ascending order
+ * @list: the list
+ *
+ */
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *temp;
+    int index;
+
+    if(list == NULL && index > 0)
+    
+
+    /*for (*list) 
+        if list->prev > list->node
+            temp_node = list->prev
+            list->prev = list_node
+            list_node = temp_node
+
+    */
+}
